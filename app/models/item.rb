93dea1fb87ddfaf3813e6e0074b4f73b7e0b43f7
Dima <dimitriy@ukr.net>
2014-03-18 00:01:44 +0200

    Git with Carrierwave and one small error

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,12 +4,13 @@ class Item < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0, allow_nil: true } 
   validates :name, :description, presence: true
   
+  mount_uploader :image, ImageUploader
   has_many :positions
   has_many :carts, through: :positions
   
   has_many :comments, as: :commentable
   
-  #  has_one :image
+  # has_one :image
   # accepts_nested_attributes_for :image
   after_initialize {  }
   after_save {  }
